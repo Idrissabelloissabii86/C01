000f1bd67117833c0ffecdc6f87103ea31b24a77
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-14 00:31:51 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #ce script affiche les arguments passés ligne par ligne
 echo $@
-echo "Argument 1: $1"
-shift
-echo "Argument 2: $2"
+while [ $# -gt 0 ]; do
+    echo "Argument 1: $1"
+    shift
+    echo "Argument 2: $1"
+    exit 0
+done
