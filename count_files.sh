d4b8fe4271ca4536e0f1ba17e9683c36e42e97bc
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-13 21:23:05 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -5,13 +5,9 @@
 read myfolder
 
 # La condition vérifie d'abord l'existence du dossier et lance la boucle qui va lister et compter les fichiers grace ls et wc -l
-
-if [ -d $myfolder ]; then
     while files_number=$(ls $myfolder | wc -l); do
     echo "Le dossier myfolder contient $files_number fichier(s)."
     exit 0
     done
-else
-    echo "Le dossier $myfolder n'existe pas."
-fi
+
 
