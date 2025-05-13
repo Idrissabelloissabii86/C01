6e9c1ee90d17d8cc823f819ebf1e02eee70485a7
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-13 23:44:52 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ce script permet d'afficher ligne par ligne les arguments qui accompagne le script
-read argument
-echo "Argument 1: $1"
-echo "Argument 2: $2"
+for arg in "$@"; do
+  echo "Argument 1: $1"
+  echo "Argument 2: $2"
+done
