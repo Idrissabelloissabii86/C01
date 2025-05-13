179ba1a839062a9c2204cb360952b75faf7797da
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-13 21:46:59 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -2,7 +2,7 @@
 # Ce script permet d'afficher les d'afficher les arguments qui suivent la commande d'exécution du script
 
 # déclaration d'une variable i qui sera incrémentée en fonction du nombre d'argument
-
+read argument
 i=1
 
 # la boucle qui permet d'afficher les arguments ligne par ligne
