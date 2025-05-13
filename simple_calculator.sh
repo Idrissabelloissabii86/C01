0c0a1f68fce1532d76fb877c64898de0ba450bd4
BELLO ISSA IDRISSA <64420230+Idrissabelloissabii86@users.noreply.github.com>
2025-05-13 21:27:39 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -6,10 +6,4 @@ read num1 num2 op
 # Effectue l'opération
 if [ "$op" = "+" ]; then
      echo "Resultat : $((num1 + num2))"
-elif [ "$op" = "-" ]; then
-     echo "Resultat : $((num1 - num2))"
-elif [ "$op" = "*" ]; then
-    echo "Resultat : $((num1 * num2))"
-else
-    echo "Resultat : $((num1 / num2))"
 fi
